42f948d5a010d3a12e56924ad558d9b93cf36484
thesinglecloud <hanfeng@zdns.cn>
2019-05-22 02:53:12 +0000

    add support to register node with agent

--- main.go
+++ main.go
@@ -19,8 +19,15 @@ limitations under the License.
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"net"
+	"net/http"
+	"time"
 
 	"github.com/zdnscloud/cement/log"
 
@@ -30,12 +37,55 @@ import (
 	"google.golang.org/grpc/reflection"
 )
 
+func registerWithClusterAgent(clusterAgentAddr, nodeName, nodeAddr string) error {
+	client := &http.Client{}
+	url := fmt.Sprintf("http://%s/apis/agent.zcloud.cn/v1/nodeagents", clusterAgentAddr)
+	requestBody, _ := json.Marshal(map[string]interface{}{
+		"name":    nodeName,
+		"address": nodeAddr,
+	})
+	req, _ := http.NewRequest("POST", url, bytes.NewBuffer(requestBody))
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	if resp.StatusCode != 201 {
+		return errors.New(string(body))
+	}
+
+	return nil
+}
+
 func main() {
 	var addr string
+	var clusterAgentAddr string
+	var nodeName string
+
 	flag.StringVar(&addr, "listen", ":80", "server listen address")
+	flag.StringVar(&clusterAgentAddr, "cluster", "cluster-agent.zcloud.svc", "server listen address")
+	flag.StringVar(&nodeName, "node", "", "node name this node agent resides")
 	flag.Parse()
 
 	log.InitLogger(log.Debug)
+
+	if nodeName == "" {
+		log.Fatalf("node name isn't specified")
+	} else if clusterAgentAddr == "" {
+		log.Fatalf("cluster agent addr isn't specified")
+	}
+	for {
+		err := registerWithClusterAgent(clusterAgentAddr, nodeName, addr)
+		if err != nil {
+			log.Errorf("register to cluster agent failed:%s, start to retry", err.Error())
+			<-time.After(10 * time.Second)
+		} else {
+			log.Infof("register to cluster agent %s succeed", clusterAgentAddr)
+			break
+		}
+	}
 
 	svr := server.NewServer()
 	grpcServer := grpc.NewServer()
